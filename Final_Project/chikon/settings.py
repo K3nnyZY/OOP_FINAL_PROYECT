aa01a5503d7cd90b4b054c5ca2c7ad6b8121a8a1
Kenny Zhu Ye <kennyzhuye@Kennys-MacBook-Air.local>
2022-11-16 07:23:04 -0500

    16/11/22

--- Final_Project/chikon/settings.py
+++ Final_Project/chikon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'store',
     'carts',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,8 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
