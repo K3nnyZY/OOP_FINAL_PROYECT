d95d19bf3a8cd0afe6a6288488c2983f3d9b163e
Kenny Zhu Ye <kennyzhuye@Kennys-MacBook-Air.local>
2022-11-20 14:30:20 -0500

    c

--- src/carts/views.py
+++ src/carts/views.py
@@ -87,9 +87,7 @@ def checkout(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        
-        delivery = (2*total)/100
-        grand_total = total + delivery
+    
 
     except ObjectDoesNotExist:
         pass ## solo ignora la exception
@@ -98,8 +96,6 @@ def checkout(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'delivery' : delivery,
-        'grand_total' : grand_total
     }
     return render(request, 'store/checkout.html',context)
 
